f5835ee824a3a7d401fd72cea64ee31f861f024f
Nick Kovalsky <taublast@gmail.com>
2026-02-27 10:15:46 +0300

    ren camerastate to hardwarestate

---
+++ src/Enums/HardwareState.cs
@@ -0,0 +1,16 @@
+﻿namespace DrawnUi.Camera;
+
+public enum HardwareState
+{
+    /// <summary>
+    /// Stopped
+    /// </summary>
+    Off,
+
+    /// <summary>
+    /// Running
+    /// </summary>
+    On,
+
+	Error
+}
